da160514180dd40de805d749b1558d2fd59efe1f
Nuru Dashdamirli <nurutasdemir@gmail.com>
2022-04-06 12:54:21 +0400

    separate class for string finder

--- BinaryStringDetector/MainForm.cs
+++ BinaryStringDetector/MainForm.cs
@@ -11,64 +11,19 @@ namespace BinaryStringDetector
         public MainForm()
         {
             InitializeComponent();
-            notFoundText = new List<String> {"(not found)"};
-        }
-
-        public List<String> getValidStringsFromFile(String fileLocation, int minimalDetectedStringLength = 7, bool unicodeOverAscii = false, bool onlyNullTerminated = false)
-        {
-            byte[] fileContent;
-            List<String> validStringList = new List<String>(); 
-            
-            // handle file
-            try {fileContent = System.IO.File.ReadAllBytes(fileLocation);}
-            catch {return validStringList;}
-            
-            String validString = "";
-            int validStrLenCounter = 0;
-            long startingAdress = 0;
-
-            for (int i = 0; i < fileContent.Length - System.Convert.ToInt32(unicodeOverAscii); i++)
-            {
-                byte firstByte = fileContent[i];
-
-                //this logic works because of by unicode I mean ascii unicode: 0x0000-0x00FF
-                //ascii or unicode logic in next 2 lines
-                byte secondByte = 0;
-                if (unicodeOverAscii) secondByte = fileContent[++i];
-
-                if ( secondByte == 0
-                     && ((firstByte < 127 && firstByte > 31)
-                     || (firstByte == 9 || firstByte == 10 || firstByte == 13))
-                    )
-                {
-                    validString += System.Convert.ToChar(firstByte);
-                    validStrLenCounter++;
-                }
-                else
-                {
-                    if (validStrLenCounter > minimalDetectedStringLength && (!onlyNullTerminated || firstByte == 0))
-                    {
-                        // print with hexadecimal location of found string
-                        validStringList.Add(startingAdress.ToString("X2").PadLeft(8, '0') + " - " + validString);
-                    }
-                    validStrLenCounter = 0;
-                    validString = "";
-                    startingAdress = i;
-                }
-            }
-
-            // in case file is finished and last else is 
-            if (!onlyNullTerminated && validString.Length != 0) validStringList.Add(startingAdress.ToString("X2").PadLeft(8, '0') + " - " + validString);
-            
-            return validStringList;
+            notFoundText = new List<String> {"(not found)"}; //empty listbox source if nothing found
         }
 
         private void prepareListboxes(String filename)
         {
             int minimalValidStrLen = (int)numUpDownMinLen.Value;
-            listBoxAsciiStrings.DataSource = getValidStringsFromFile(filename, minimalValidStrLen, false, checkBoxOnlyNullTerminated.Checked);
+            
+            //ascii strings
+            listBoxAsciiStrings.DataSource = StringFinder.getValidStringsFromFile(filename, minimalValidStrLen, false, checkBoxOnlyNullTerminated.Checked);
             if (listBoxAsciiStrings.Items.Count == 0) listBoxAsciiStrings.DataSource = notFoundText;
-            listBoxUnicodeStrings.DataSource = getValidStringsFromFile(filename, minimalValidStrLen, true, checkBoxOnlyNullTerminated.Checked);
+
+            //unicode strings only in range 0x0000-0x00FF
+            listBoxUnicodeStrings.DataSource = StringFinder.getValidStringsFromFile(filename, minimalValidStrLen, true, checkBoxOnlyNullTerminated.Checked);
             if (listBoxUnicodeStrings.Items.Count == 0) listBoxUnicodeStrings.DataSource = notFoundText;
         }
 
---
+++ BinaryStringDetector/StringFinder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryStringDetector
+{
+    class StringFinder
+    {
+        public static List<String> getValidStringsFromFile(String fileLocation, int minimalStrLen = 7, bool isUnicodeOnlyNeeded = false, bool isNullTerminationNeeded = false)
+        {
+            byte[] fileContent;
+            List<String> validStringList = new List<String>();
+
+            // handle file
+            try { fileContent = System.IO.File.ReadAllBytes(fileLocation); }
+            catch { return validStringList; }
+
+            String validString = "";
+            int validStrLenCounter = 0;
+            long startingAdress = 0;
+
+            // we subtract 1 because reading 2 characters in unicode mode will cause indexOutOfBounds
+            for (int i = 0; i < fileContent.Length - System.Convert.ToInt32(isUnicodeOnlyNeeded); i++)
+            {
+                byte firstByte = fileContent[i];
+
+                // this logic works because of by unicode I mean ascii unicode: 0x0000-0x00FF
+                // ascii or unicode logic in next 2 lines
+                byte secondByte = 0;
+                if (isUnicodeOnlyNeeded) secondByte = fileContent[++i];
+
+                if (secondByte == 0
+                     && ((firstByte < 127 && firstByte > 31)
+                     || (firstByte == 9 || firstByte == 10 || firstByte == 13))
+                    )
+                {
+                    validString += System.Convert.ToChar(firstByte);
+                    validStrLenCounter++;
+                }
+                else
+                {
+                    if (validStrLenCounter > minimalStrLen && (!isNullTerminationNeeded || firstByte == 0))
+                    {
+                        // print with hexadecimal location of found string
+                        validStringList.Add(startingAdress.ToString("X2").PadLeft(8, '0') + " - " + validString);
+                    }
+                    validStrLenCounter = 0;
+                    validString = "";
+                    startingAdress = i;
+                }
+            }
+
+            // in case EOF reached and non-null terminated strings required (otherwise if last character is printable it wont appear)
+            if (!isNullTerminationNeeded && validString.Length != 0) validStringList.Add(startingAdress.ToString("X2").PadLeft(8, '0') + " - " + validString);
+
+            return validStringList;
+        }
+    }
+}
